e7d4cf2c560c2ec93467b5e16c83042aadce44b8
FalconGoat <falcongoat@tanzenjetzt.de>
2018-03-06 16:52:46 +0100

    Move color definitions into own file

--- lib/prompt.bash
+++ lib/prompt.bash
@@ -40,16 +40,3 @@ __funkybash() {
 
 		printf "$path"
 	}
-
-#	Colorscheme
-#	..........................................................................
-	_termBg=16
-
-	_userFg=236
-	_userBg=250
-	_atFg=234
-	_atBg=248
-	_hostFg=232
-	_hostBg=246
-	_pathBg=236
-	_pathFg=246
