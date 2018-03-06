0867924e0bb717cfdac3958f1aa3b0451fb765b6
FalconGoat <falcongoat@tanzenjetzt.de>
2018-03-06 02:57:45 +0100

    Add functions for better FG/BG color setting

--- profile.bash
+++ profile.bash
@@ -6,16 +6,16 @@
 
 #	Check, if the script was sourced or called directly
 #	Set $SCRIPTPATH accordingly to reflect PWD
-if [[ $_ != $0 ]]; then
-	SCRIPTPATH="$(dirname $BASH_SOURCE)"    # sourced
-else
-	SCRIPTPATH="$(dirname $0)"              # called directly
-fi 
+	if [[ $_ != $0 ]]; then
+		SCRIPTPATH="$(dirname $BASH_SOURCE)"    # sourced
+	else
+		SCRIPTPATH="$(dirname $0)"              # called directly
+	fi
 
 #	Create variable storage if not present & clear it
-STORAGE="/tmp/.powerline"
-[ ! -d "$STORAGE" ] && mkdir "$STORAGE" > /dev/null
-rm -rf ${STORAGE}/*
+	STORAGE="/tmp/.powerline"
+	[ ! -d "$STORAGE" ] && mkdir "$STORAGE" > /dev/null
+	rm -rf ${STORAGE}/*
 
 
 #	-----------------------------
@@ -23,6 +23,10 @@ rm -rf ${STORAGE}/*
 #	-----------------------------
 
 #   Add color to terminal
-export TERM=xterm-256color
-export CLICOLOR=$TERM
-source $SCRIPTPATH/lib/colors.bash
+#	..........................................................................
+	export TERM=xterm-256color
+	export CLICOLOR=$TERM
+	source $SCRIPTPATH/lib/colors.bash
+
+#	Change Prompt
+#	..........................................................................
